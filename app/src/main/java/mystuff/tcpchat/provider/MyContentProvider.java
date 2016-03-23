b15fa17e0a66028034940d15293371190065c6f0
st127250 <marco.reggio@edu.unito.it>
2016-03-23 12:01:31 +0100

    Writing content provider

--- app/src/main/java/mystuff/tcpchat/provider/MyContentProvider.java
+++ app/src/main/java/mystuff/tcpchat/provider/MyContentProvider.java
@@ -2,18 +2,37 @@ package mystuff.tcpchat.provider;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 public class MyContentProvider extends ContentProvider {
 
     private static final String AUTHORITY ="com.ebookfrenzy.database.provider.MyContentProvider";
-    private static final String TABLE = "table";
-    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + TABLE);
-    public static final int WHOLE_TABLE = 1;
-    public static final int ROW_TABLE = 2;
+    private static final String MESSAGES_TABLE = "table";
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + MESSAGES_TABLE);
+    public static final int  MESSAGES= 1;
+    public static final int MESSAGE_ID = 2;
+    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    /**
+     * Database
+     */
+    private SQLiteDatabase db;
+    static final String DATABASE_NAME = "TCPChat";
+    static final String MESSAGES_TABLE_NAME = "Messages";
+    static final int DATABASE_VERSION = 1;
+    static final String CREATE_DB_TABLE =
+        " CREATE TABLE " +
+        MESSAGES_TABLE_NAME +
+        " (_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+        " name TEXT NOT NULL, " +
+        " grade TEXT NOT NULL);";
 
     public MyContentProvider() {
+        uriMatcher.addURI(AUTHORITY, MESSAGES_TABLE, MESSAGES);
+        uriMatcher.addURI(AUTHORITY, MESSAGES_TABLE + "/#", MESSAGE_ID);
     }
 
     @Override
