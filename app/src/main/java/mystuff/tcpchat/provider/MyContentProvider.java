03dd8c39fc0b091095042a332a4dfd4a79061dc3
st127250 <marco.reggio@edu.unito.it>
2016-03-23 15:11:35 +0100

    Some efforts to MyContentProvider, yet to be tested. Added Strings provider but it's empty.

--- app/src/main/java/mystuff/tcpchat/provider/MyContentProvider.java
+++ app/src/main/java/mystuff/tcpchat/provider/MyContentProvider.java
@@ -1,41 +1,55 @@
 package mystuff.tcpchat.provider;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.text.TextUtils;
+
+import java.sql.SQLException;
+import java.util.HashMap;
 
 public class MyContentProvider extends ContentProvider {
 
-    private static final String AUTHORITY ="com.ebookfrenzy.database.provider.MyContentProvider";
-    private static final String MESSAGES_TABLE = "table";
-    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + MESSAGES_TABLE);
-    public static final int  MESSAGES= 1;
-    public static final int MESSAGE_ID = 2;
-    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    static final String PROVIDER_NAME = "mystuff.tcpchat.provider.AllMessages";
+    static final String URL = "content://" + PROVIDER_NAME + "/messages";
+    static final Uri CONTENT_URI = Uri.parse(URL);
+
+    static final String _ID = "_id";
+    static final String SENDER = "sender";
+    static final String RECEIVER = "receiver";
+    static final String TIME = "time";
+
+    private static HashMap<String, String> STUDENTS_PROJECTION_MAP;
+
+    static final int MESSAGES = 1;
+    static final int MESSAGE_ID = 2;
+
+    static final UriMatcher uriMatcher;
+    static{
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(PROVIDER_NAME, "messages", MESSAGES);
+        uriMatcher.addURI(PROVIDER_NAME, "messages/#", MESSAGE_ID);
+    }
 
     /**
-     * Database
+     * Database specific constant declarations
      */
     private SQLiteDatabase db;
-    static final String DATABASE_NAME = "TCPChat";
-    static final String MESSAGES_TABLE_NAME = "Messages";
+    static final String DATABASE_NAME = "AllMessages";
+    static final String MESSAGES_TABLE_NAME = "messages";
     static final int DATABASE_VERSION = 1;
     static final String CREATE_DB_TABLE =
-        " CREATE TABLE " +
-        MESSAGES_TABLE_NAME +
-        " (_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-        " name TEXT NOT NULL, " +
-        " grade TEXT NOT NULL);";
-
-    public MyContentProvider() {
-        uriMatcher.addURI(AUTHORITY, MESSAGES_TABLE, MESSAGES);
-        uriMatcher.addURI(AUTHORITY, MESSAGES_TABLE + "/#", MESSAGE_ID);
-    }
+            " CREATE TABLE " + MESSAGES_TABLE_NAME +
+                    " (_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    " name TEXT NOT NULL, " +
+                    " grade TEXT NOT NULL);";
 
     /**
      * Helper class that actually creates and manages
@@ -47,7 +61,8 @@ public class MyContentProvider extends ContentProvider {
         }
 
         @Override
-        public void onCreate(SQLiteDatabase db) {
+        public void onCreate(SQLiteDatabase db)
+        {
             db.execSQL(CREATE_DB_TABLE);
         }
 
@@ -58,43 +73,132 @@ public class MyContentProvider extends ContentProvider {
         }
     }
 
-
     @Override
     public boolean onCreate() {
-        db = new DatabaseHelper(getContext()).getWritableDatabase();
-        return (db == null);
-    }
+        Context context = getContext();
+        DatabaseHelper dbHelper = new DatabaseHelper(context);
 
-    @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
-        // Implement this to handle requests to delete one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    @Override
-    public String getType(Uri uri) {
-        // TODO: Implement this to handle requests for the MIME type of the data
-        // at the given URI.
-        throw new UnsupportedOperationException("Not yet implemented");
+        /**
+         * Create a write able database which will trigger its
+         * creation if it doesn't already exist.
+         */
+        db = dbHelper.getWritableDatabase();
+        return (db != null);
     }
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        // TODO: Implement this to handle requests to insert a new row.
-        throw new UnsupportedOperationException("Not yet implemented");
+        /**
+         * Add a new student record
+         */
+        long rowID = db.insert(	MESSAGES_TABLE_NAME, "", values);
+
+        /**
+         * If record is added successfully
+         */
+
+        if (rowID > 0){
+            Uri _uri = ContentUris.withAppendedId(CONTENT_URI, rowID);
+            getContext().getContentResolver().notifyChange(_uri, null);
+            return _uri;
+        }
+        return null;
     }
 
     @Override
-    public Cursor query(Uri uri, String[] projection, String selection,
-                        String[] selectionArgs, String sortOrder) {
-        // TODO: Implement this to handle query requests from clients.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public Cursor query(Uri uri, String[] projection, String selection,String[] selectionArgs, String sortOrder) {
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        qb.setTables(MESSAGES_TABLE_NAME);
+
+        switch (uriMatcher.match(uri)) {
+            case MESSAGES:
+                qb.setProjectionMap(STUDENTS_PROJECTION_MAP);
+                break;
+
+            case MESSAGE_ID:
+                qb.appendWhere( _ID + "=" + uri.getPathSegments().get(1));
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        if (sortOrder == null || sortOrder.equals("")){
+            /**
+             * By default sort on student names
+             */
+            sortOrder = TIME;
+        }
+        Cursor c = qb.query(db,	projection,	selection, selectionArgs,null, null, sortOrder);
+
+        /**
+         * register to watch a content URI for changes
+         */
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+        return c;
     }
 
     @Override
-    public int update(Uri uri, ContentValues values, String selection,
-                      String[] selectionArgs) {
-        // TODO: Implement this to handle requests to update one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        int count;
+
+        switch (uriMatcher.match(uri)){
+            case MESSAGES:
+                count = db.delete(MESSAGES_TABLE_NAME, selection, selectionArgs);
+                break;
+
+            case MESSAGE_ID:
+                String id = uri.getPathSegments().get(1);
+                count = db.delete( MESSAGES_TABLE_NAME, _ID +  " = " + id +
+                        (!TextUtils.isEmpty(selection) ? " AND (" + selection + ')' : ""), selectionArgs);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        int count;
+
+        switch (uriMatcher.match(uri)){
+            case MESSAGES:
+                count = db.update(MESSAGES_TABLE_NAME, values, selection, selectionArgs);
+                break;
+
+            case MESSAGE_ID:
+                count = db.update(MESSAGES_TABLE_NAME, values, _ID + " = " + uri.getPathSegments().get(1) +
+                        (!TextUtils.isEmpty(selection) ? " AND (" +selection + ')' : ""), selectionArgs);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri );
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        switch (uriMatcher.match(uri)){
+            /**
+             * Get all student records
+             */
+            case MESSAGES:
+                return "vnd.android.cursor.dir/vnd.example.messages";
+
+            /**
+             * Get a particular student
+             */
+            case MESSAGE_ID:
+                return "vnd.android.cursor.item/vnd.example.messages";
+
+            default:
+                throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
     }
 }
