a974362b1078aac0afea1a9384a6b78f79e5be95
st127250 <marco.reggio@edu.unito.it>
2016-03-29 12:02:02 +0200

    Added provider, to be tested

--- app/src/main/java/mystuff/tcpchat/contentprovider/ChatMessagesContentProvider.java
+++ app/src/main/java/mystuff/tcpchat/contentprovider/ChatMessagesContentProvider.java
@@ -8,6 +8,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.text.TextUtils;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -44,26 +45,6 @@ public class ChatMessagesContentProvider extends ContentProvider {
         return false;
     }
 
-
-    @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
-        // Implement this to handle requests to delete one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    @Override
-    public String getType(Uri uri) {
-        // TODO: Implement this to handle requests for the MIME type of the data
-        // at the given URI.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    @Override
-    public Uri insert(Uri uri, ContentValues values) {
-        // TODO: Implement this to handle requests to insert a new row.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
 
@@ -95,8 +76,7 @@ public class ChatMessagesContentProvider extends ContentProvider {
         }
 
         SQLiteDatabase db = databaseHelper.getWritableDatabase();
-        Cursor cursor = queryBuilder.query(db, projection, selection,
-                selectionArgs, null, null, sortOrder);
+        Cursor cursor = queryBuilder.query(db, projection, selection, selectionArgs, null, null, sortOrder);
         // make sure that potential listeners are getting notified
         cursor.setNotificationUri(getContext().getContentResolver(), uri);
 
@@ -104,11 +84,98 @@ public class ChatMessagesContentProvider extends ContentProvider {
     }
 
     @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        SQLiteDatabase db = databaseHelper.getWritableDatabase();
+        int uritype = sURIMatcher.match(uri);
+        long id;
+
+        switch(uritype){
+            case MESSAGES:{
+                id = db.insert(MessagesTable.TABLE_MESSAGES, null, values);
+                break;
+            }
+            default:
+                throw new IllegalArgumentException("insert: PATH NOT KNOWN");
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return Uri.parse(BASE_PATH + "/" + id);
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        int uriType = sURIMatcher.match(uri);
+        SQLiteDatabase db = databaseHelper.getWritableDatabase();
+        int rowsDeleted = 0;
+        switch (uriType) {
+            case MESSAGES:
+                rowsDeleted = db.delete(MessagesTable.TABLE_MESSAGES, selection, selectionArgs);
+                break;
+            case MESSAGE_SENDER:
+                String sender = uri.getLastPathSegment();
+                if (TextUtils.isEmpty(selection)) {
+                    rowsDeleted = db.delete(MessagesTable.TABLE_MESSAGES, MessagesTable.COLUMN_SENDER + "=" + sender, null);
+                } else {
+                    rowsDeleted = db.delete(MessagesTable.TABLE_MESSAGES,
+                            MessagesTable.COLUMN_SENDER + "=" + sender + " and " + selection,
+                            selectionArgs);
+                }
+                break;
+            case MESSAGE_RECEIVER:
+                String receiver = uri.getLastPathSegment();
+                if (TextUtils.isEmpty(selection)) {
+                    rowsDeleted = db.delete(MessagesTable.TABLE_MESSAGES, MessagesTable.COLUMN_RECEIVER + "=" + receiver, null);
+                } else {
+                    rowsDeleted = db.delete(MessagesTable.TABLE_MESSAGES,
+                            MessagesTable.COLUMN_RECEIVER + "=" + receiver + " and " + selection,
+                            selectionArgs);
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return rowsDeleted;
+    }
+
+    @Override
     public int update(Uri uri, ContentValues values, String selection,
                       String[] selectionArgs) {
-        // TODO: Implement this to handle requests to update one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+        int uriType = sURIMatcher.match(uri);
+        SQLiteDatabase db = databaseHelper.getWritableDatabase();
+        int rowsUpdated = 0;
+        switch (uriType) {
+            case MESSAGES:
+                rowsUpdated = db.update(MessagesTable.TABLE_MESSAGES, values, selection, selectionArgs);
+                break;
+            case MESSAGE_SENDER:
+                String sender = uri.getLastPathSegment();
+                if (TextUtils.isEmpty(selection)) {
+                    rowsUpdated = db.update(MessagesTable.TABLE_MESSAGES, values, MessagesTable.COLUMN_SENDER + "=" + sender, null);
+                } else {
+                    rowsUpdated = db.update(MessagesTable.TABLE_MESSAGES, values,
+                            MessagesTable.COLUMN_SENDER + "=" + sender + " and " + selection,
+                            selectionArgs);
+                }
+                break;
+            case MESSAGE_RECEIVER:
+                String receiver = uri.getLastPathSegment();
+                if (TextUtils.isEmpty(selection)) {
+                    rowsUpdated = db.update(MessagesTable.TABLE_MESSAGES, values, MessagesTable.COLUMN_RECEIVER + "=" + receiver, null);
+                } else {
+                    rowsUpdated = db.update(MessagesTable.TABLE_MESSAGES, values,
+                            MessagesTable.COLUMN_RECEIVER + "=" + receiver + " and " + selection,
+                            selectionArgs);
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return rowsUpdated;
     }
+
+
 
     private void checkColumns(String[] projection) {
         String[] available = { MessagesTable.COLUMN_DATE,
@@ -123,4 +190,16 @@ public class ChatMessagesContentProvider extends ContentProvider {
             }
         }
     }
+
+    @Override
+    public String getType(Uri uri) {
+        // TODO: Implement this to handle requests for the MIME type of the data
+        // at the given URI.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+
+
+
 }
